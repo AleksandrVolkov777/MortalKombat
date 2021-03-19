3a5045ac20a2ec18a6a43c5fef5a462a2a4cf9c5
Александр <volk77793@gmail.com>
2021-03-19 21:56:42 +0300

    5com

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 	$('#app').vide({
-		mp4: 'video/video.mp4',
-		webm: 'video/video.webm',
-		ogv: 'video/video.ogv',
+		mp4: '../video/video.mp4',
+		webm: '../video/video.webm',
+		ogv: '../video/video.ogv',
 	});
 });
